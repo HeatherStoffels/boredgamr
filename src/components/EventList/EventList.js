b625f713d05f3ce351b19d7552e0d2babcee7440
Heather Stoffels <Umdstarlet@gmail.com>
2020-05-30 12:34:08 -0500

    button to details page working

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { Link } from "react-router-dom";
 
 class EventList extends Component {
+  handleClick = () => {};
+
   render() {
     return (
       <div>
         <h2>Event List</h2>
         <ul>
-            {this.props.events.map(event=>(
-                <li key={event.id}>{event.username} - {event.name} - {event.date_time}</li>
-            ))}
+          {this.props.events.map((event) => (
+            <li key={event.id}>
+              {event.username} - {event.name} - {event.date_time} -
+              <Link to="/details">
+                <button onClick={this.handleClick}>More Info</button>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     );
