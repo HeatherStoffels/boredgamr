20f5191b912a75f48bd6b5c76b3579140877cd54
Heather Stoffels <Umdstarlet@gmail.com>
2020-06-03 14:17:07 -0500

    cards working, in one column.

--- server/routes/newEvent.router.js
+++ server/routes/newEvent.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // POST /create
 // Body: {host_id: [host id], game_id: [game id], date_time: [date_time string]}
-router.post("/", (req, res) => {
+router.post("/",  (req, res) => {
   console.log("request received in event get");
   let query = `INSERT INTO events ("host_id", "game_id", "date_time") VALUES ($1, $2, $3)`;
   let values = [req.body.host_id, req.body.game_id, req.body.date_time];
--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -3,38 +3,59 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import Button from "@material-ui/core/Button";
+import { Card, CardImg, CardBody, CardTitle, CardDeck, Row, Col } from "reactstrap";
+import "./EventList.css"
 
 class EventList extends Component {
   componentDidMount() {
     this.props.dispatch({ type: "GET_ALL_GAMES" });
   }
   handleClick = (e) => {
-    this.props.dispatch({ type: "GET_EVENT_BY_ID", payload: e.currentTarget.value });
+    this.props.dispatch({
+      type: "GET_EVENT_BY_ID",
+      payload: e.currentTarget.value,
+    });
   };
 
   render() {
     return (
-      <div>
+      <div className="eventList">
         <h2>Event List</h2>
 
         <ul>
-          {this.props.events.map((event) => (
-            <li key={event.events_id}>
-              Host: {event.username} - {event.name} -{" "}
-              {moment(event.date_time).format("MMMM Do YYYY, h:mm a")}{" "}
-              <img src={event.picture} width="100px" alt={event.game_name} />
-              <Link to="/details">
-                <Button
-                  size="small"
-                  variant="contained"
-                  value={parseInt(event.events_id)}
-                  onClick={this.handleClick}
-                >
-                  More Info
-                </Button>
-              </Link>
-            </li>
-          ))}
+          {this.props.events.map((event) => {
+            return (
+     
+          <CardDeck>
+              <Card className="card" body outline color="primary">
+                <CardBody>
+                <CardTitle>{event.name}</CardTitle> 
+                  Host: {event.username} <br/>
+                  {moment(event.date_time).format("MMMM Do YYYY, h:mm a")}{" "}
+                </CardBody>
+                <CardImg
+                  src={event.picture}
+                  top
+                  width="100px"
+                  style={{ width: "50%", height: "40%" }}
+                  alt={event.game_name}
+                />
+                <br/>
+                <Link to="/details">
+                  <Button
+                    size="small"
+                    variant="contained"
+                    value={parseInt(event.events_id)}
+                    onClick={this.handleClick}
+                  >
+                    More Info
+                  </Button>
+                </Link>
+              </Card>
+              </CardDeck>
+           
+            );
+          })}
         </ul>
       </div>
     );
